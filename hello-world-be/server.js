05bc4846768cd5eb08e046c95679a59a85f65fd4
Johan <johan@codertrove.com>
2024-04-16 11:58:49 +0700

    CHORE: Refactor to run redis as a StatefulSet

--- hello-world-be/server.js
+++ hello-world-be/server.js
@@ -1,12 +1,9 @@
 const http = require('http');
 const redis = require('redis');
-const { v4: uuidv4 } = require('uuid');
-
-const uniqueID = uuidv4();  // Generates a unique ID for each instance
 
 const host = process.env.REDIS_HOST || 'redis-service';
-
-console.log('process.env.REDIS_HOST: ', process.env.REDIS_HOST)
+const podName = process.env.POD_NAME || 'not a pod';
+const podIp = process.env.POD_IP || 'not a pod';
 
 const client = redis.createClient({
     url: `redis://${host}:6379`
@@ -14,12 +11,14 @@ const client = redis.createClient({
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 
+let requestCount = 0;
+
 const server = http.createServer(async (req, res) => {
     if (req.url === '/') {
         try {
             const reply = await client.incr('counter');
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`Hello world from node instance ${uniqueID}! This server (cluster) has been visited ${reply} times.\n`);
+            res.end(`Hello world! from node instance: ${podName} at IP: ${podIp}, This node has been visited ${++requestCount} times and the cluster has been visited a total of ${reply} times.\n`);
         } catch (err) {
             console.error('Redis error:', err);
             res.writeHead(500);
